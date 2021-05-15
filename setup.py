d5952fc4830719b2c4b32e2b1f76cad1381f391a
Michael Reca <michael.reca@gmail.com>
2021-05-14 21:54:40 -0400

    version 0.0.4

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pip-from-git-demo',
-      version='0.0.3',
+      version='0.0.4',
       description='Demo package to illustrate how to setup and install different package versions with pip, from github',
       author='Michael L. Reca',
       author_email='michael.reca@gmail.com',
