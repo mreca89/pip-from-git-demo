180c33093aece5dc297a29636601ff0ab638a86f
Michael Reca <michael.reca@gmail.com>
2021-05-14 20:56:58 -0400

    minor change in setup.py (author +middlename) - to see "Releases" vs "Tags" difference in github

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(name='pip-from-git-demo',
-      version='0.0.1',
+      version='0.0.3',
       description='Demo package to illustrate how to setup and install different package versions with pip, from github',
-      author='Michael Reca',
+      author='Michael L. Reca',
       author_email='michael.reca@gmail.com',
       url='https://github.com/mreca89/pip-from-git-demo'
 )
