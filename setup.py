8e1da99a6ba1bfd21585dbbcde221b8612d129ca
Michael Reca <michael.reca@gmail.com>
2021-05-14 20:51:19 -0400

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pip-from-git-demo',
-      version='0.0.0',
+      version='0.0.1',
       description='Demo package to illustrate how to setup and install different package versions with pip, from github',
       author='Michael Reca',
       author_email='michael.reca@gmail.com',
